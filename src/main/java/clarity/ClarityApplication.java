569fbba2493eaaaddb83c671fb4c4054ba904de0
Olle Törnström <toernstroem@synyx.de>
2025-10-10 16:21:15 +0200

    chore: Move up one package

--- src/main/java/clarity/ClarityApplication.java
+++ src/main/java/clarity/ClarityApplication.java
@@ -1,4 +1,4 @@
-package clarity.clarity;
+package clarity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/clarity/ClarityApplicationTests.java
+++ src/test/java/clarity/ClarityApplicationTests.java
@@ -1,4 +1,4 @@
-package clarity.clarity;
+package clarity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
