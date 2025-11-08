5cd47a691a34ed50a9e686f1ad9daea62ccc8ff1
Olle Törnström <olle@studiomediatech.com>
2025-11-08 10:52:14 +0100

    Make immutable

--- src/main/java/clarity/management/domain/BrokerAttributes.java
+++ src/main/java/clarity/management/domain/BrokerAttributes.java
@@ -1,6 +1,7 @@
 package clarity.management.domain;
 
 import java.io.Serial;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.lang.Nullable;
 
@@ -8,18 +9,43 @@ public class BrokerAttributes extends ConcurrentHashMap<String, Object> {
 
   @Serial private static final long serialVersionUID = 1L;
 
+  private BrokerAttributes() {
+    super();
+  }
+
+  private BrokerAttributes(BrokerAttributes other) {
+    super(other);
+  }
+
+  private BrokerAttributes(Map<String, Object> map) {
+    super(map);
+  }
+
+  public static BrokerAttributes empty() {
+    return new BrokerAttributes();
+  }
+
   public BrokerAttributes with(String key, Object value) {
     put(key, value);
-    return this;
+    return new BrokerAttributes(this);
   }
 
   public BrokerAttributes without(String key) {
     remove(key);
-    return this;
+    return new BrokerAttributes(this);
   }
 
   public BrokerAttributes withNullable(String key, @Nullable Object maybe) {
     if (maybe != null) return with(key, maybe);
     else return without(key);
+  }
+
+  protected BrokerAttributes withAll(Map<String, Object> attributes) {
+    putAll(attributes);
+    return new BrokerAttributes(this);
+  }
+
+  protected BrokerAttributes replace(Map<String, Object> attributes) {
+    return new BrokerAttributes(attributes);
   }
 }
