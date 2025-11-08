8855b878e6152de409686b7defdde07a23ee24c9
Olle Törnström <olle@studiomediatech.com>
2025-11-08 10:11:36 +0100

    Can remove and add

--- src/main/java/clarity/management/domain/BrokerAttributes.java
+++ src/main/java/clarity/management/domain/BrokerAttributes.java
@@ -2,6 +2,7 @@ package clarity.management.domain;
 
 import java.io.Serial;
 import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.lang.Nullable;
 
 public class BrokerAttributes extends ConcurrentHashMap<String, Object> {
 
@@ -10,5 +11,15 @@ public class BrokerAttributes extends ConcurrentHashMap<String, Object> {
   public BrokerAttributes with(String key, Object value) {
     put(key, value);
     return this;
+  }
+
+  public BrokerAttributes without(String key) {
+    remove(key);
+    return this;
+  }
+
+  public BrokerAttributes withNullable(String key, @Nullable Object maybe) {
+    if (maybe != null) return with(key, maybe);
+    else return without(key);
   }
 }
