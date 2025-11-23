8bd5f43d02c246e09f3f6a66f859a7ad11f4ca2c
Olle Törnström <olle@studiomediatech.com>
2025-11-23 16:25:39 +0100

    Api endpoint

--- src/main/java/clarity/infrastructure/api/ApiController.java
+++ src/main/java/clarity/infrastructure/api/ApiController.java
@@ -1,5 +1,6 @@
 package clarity.infrastructure.api;
 
+import jakarta.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -7,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Stream;
-
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -19,101 +19,104 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import jakarta.servlet.http.HttpServletRequest;
-
 @RestController
 public class ApiController {
 
-	@Autowired
-	ApplicationContext context;
-	@Autowired
-	BeanFactory factory;
+  @Autowired ApplicationContext context;
+  @Autowired BeanFactory factory;
 
-	@GetMapping("/api")
-	public Map<String, Object> api(HttpServletRequest request) {
+  @GetMapping("/api")
+  public Map<String, Object> api(HttpServletRequest request) {
 
-		var baseURI = ServletUriComponentsBuilder.fromRequest(request).replacePath(null).replaceQuery(null).build()
-				.toUriString();
+    var baseURI =
+        ServletUriComponentsBuilder.fromRequest(request)
+            .replacePath(null)
+            .replaceQuery(null)
+            .build()
+            .toUriString();
 
-		var controllers = context.getBeansWithAnnotation(RestController.class);
+    var controllers = context.getBeansWithAnnotation(RestController.class);
 
-		for (Entry<String, Object> entry : controllers.entrySet()) {
+    for (Entry<String, Object> entry : controllers.entrySet()) {
 
-			var beanName = entry.getKey();
-			var beanInstance = entry.getValue();
-			var beanMethods = beanInstance.getClass().getDeclaredMethods();
+      var beanName = entry.getKey();
+      var beanInstance = entry.getValue();
+      var beanMethods = beanInstance.getClass().getDeclaredMethods();
 
-			var map = new LinkedHashMap<>();
-			
-			map.put("class", (Class<?>) factory.getType(beanName));
-			map.put("endpoints", parseEndpoints(beanMethods, baseURI));
-			
-			entry.setValue(map);
-		}
+      var map = new LinkedHashMap<>();
 
-		return Map.of("controllers", controllers);
-	}
+      map.put("class", (Class<?>) factory.getType(beanName));
+      map.put("endpoints", parseEndpoints(beanMethods, baseURI));
 
-	private List<Endpoint> parseEndpoints(Method[] methods, String baseURI) {
+      entry.setValue(map);
+    }
 
-		List<Endpoint> endpoints = new ArrayList<>();
+    return Map.of("controllers", controllers);
+  }
 
-		for (Method method : methods) {
+  private List<Endpoint> parseEndpoints(Method[] methods, String baseURI) {
 
-			if (method.isAnnotationPresent(GetMapping.class)) {
-				endpoints.add(parseGetEndpoint(method, baseURI, method.getAnnotation(GetMapping.class)));
+    List<Endpoint> endpoints = new ArrayList<>();
 
-			} else if (method.isAnnotationPresent(PutMapping.class)) {
-				endpoints.add(parsePutEndpoint(method, baseURI, method.getAnnotation(PutMapping.class)));
+    for (Method method : methods) {
 
-			} else if (method.isAnnotationPresent(PostMapping.class)) {
-				endpoints.add(parsePostEndpoint(method, baseURI, method.getAnnotation(PostMapping.class)));
+      if (method.isAnnotationPresent(GetMapping.class)) {
+        endpoints.add(parseGetEndpoint(method, baseURI, method.getAnnotation(GetMapping.class)));
 
-			} else if (method.isAnnotationPresent(DeleteMapping.class)) {
-				endpoints.add(parseDeleteEndpoint(method, baseURI, method.getAnnotation(DeleteMapping.class)));
-			}
-		}
+      } else if (method.isAnnotationPresent(PutMapping.class)) {
+        endpoints.add(parsePutEndpoint(method, baseURI, method.getAnnotation(PutMapping.class)));
 
-		return endpoints;
-	}
+      } else if (method.isAnnotationPresent(PostMapping.class)) {
+        endpoints.add(parsePostEndpoint(method, baseURI, method.getAnnotation(PostMapping.class)));
 
-	private Endpoint parseGetEndpoint(Method method, String baseURI, GetMapping annotation) {
-		return new Endpoint("api".equals(method.getName()) ? "self" : method.getName(),
-				formatAbsolutePath("%s/%s", baseURI, parsePathAndValueArrays(annotation.path(), annotation.value())));
-	}
+      } else if (method.isAnnotationPresent(DeleteMapping.class)) {
+        endpoints.add(
+            parseDeleteEndpoint(method, baseURI, method.getAnnotation(DeleteMapping.class)));
+      }
+    }
 
-	private Endpoint parsePutEndpoint(Method method, String baseURI, PutMapping annotation) {
-		return parsePutPostDeleteEndpoint(method, baseURI, "PUT",
-				parsePathAndValueArrays(annotation.path(), annotation.value()));
-	}
+    return endpoints;
+  }
 
-	private Endpoint parsePostEndpoint(Method method, String baseURI, PostMapping annotation) {
-		return parsePutPostDeleteEndpoint(method, baseURI, "POST",
-				parsePathAndValueArrays(annotation.path(), annotation.value()));
-	}
+  private Endpoint parseGetEndpoint(Method method, String baseURI, GetMapping annotation) {
+    return new Endpoint(
+        "api".equals(method.getName()) ? "self" : method.getName(),
+        formatAbsolutePath(
+            "%s/%s", baseURI, parsePathAndValueArrays(annotation.path(), annotation.value())));
+  }
 
-	private Endpoint parseDeleteEndpoint(Method method, String baseURI, DeleteMapping annotation) {
-		return parsePutPostDeleteEndpoint(method, baseURI, "DELETE",
-				parsePathAndValueArrays(annotation.path(), annotation.value()));
-	}
+  private Endpoint parsePutEndpoint(Method method, String baseURI, PutMapping annotation) {
+    return parsePutPostDeleteEndpoint(
+        method, baseURI, "PUT", parsePathAndValueArrays(annotation.path(), annotation.value()));
+  }
 
-	private Endpoint parsePutPostDeleteEndpoint(Method method, String baseURI, String httpMethod, String path) {
-		return new Endpoint(method.getName(), formatAbsolutePath("[" + httpMethod + "] %s/%s", baseURI, path));
-	}
+  private Endpoint parsePostEndpoint(Method method, String baseURI, PostMapping annotation) {
+    return parsePutPostDeleteEndpoint(
+        method, baseURI, "POST", parsePathAndValueArrays(annotation.path(), annotation.value()));
+  }
 
-	private String formatAbsolutePath(String template, String baseURI, String path) {
-		return template.formatted(baseURI, path).replace("//", "/").replace(":/", "://");
-	}
+  private Endpoint parseDeleteEndpoint(Method method, String baseURI, DeleteMapping annotation) {
+    return parsePutPostDeleteEndpoint(
+        method, baseURI, "DELETE", parsePathAndValueArrays(annotation.path(), annotation.value()));
+  }
 
-	private String parsePathAndValueArrays(String[] path, String[] value) {
+  private Endpoint parsePutPostDeleteEndpoint(
+      Method method, String baseURI, String httpMethod, String path) {
+    return new Endpoint(
+        method.getName(), formatAbsolutePath("[" + httpMethod + "] %s/%s", baseURI, path));
+  }
 
-		Stream<String> s1 = List.of(path).stream();
-		Stream<String> s2 = List.of(value).stream();
+  private String formatAbsolutePath(String template, String baseURI, String path) {
+    return template.formatted(baseURI, path).replace("//", "/").replace(":/", "://");
+  }
 
-		return Stream.concat(s1, s2).filter(StringUtils::hasText).distinct().findFirst().orElse("");
-	}
+  private String parsePathAndValueArrays(String[] path, String[] value) {
 
-	record Endpoint(String rel, String url) {
-	}
+    Stream<String> s1 = List.of(path).stream();
+    Stream<String> s2 = List.of(value).stream();
 
+    return Stream.concat(s1, s2).filter(StringUtils::hasText).distinct().findFirst().orElse("");
+  }
+
+  record Endpoint(String rel, String url) {}
 }
